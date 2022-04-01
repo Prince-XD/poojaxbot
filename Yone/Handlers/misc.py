57c14313ba987e34dc698801e8b61287366166f5
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2022-04-01 10:26:28 +0530

    Update misc.py

--- Yone/Handlers/misc.py
+++ Yone/Handlers/misc.py
@@ -1,9 +1,10 @@
 from math import ceil
-from typing import Dict, List
-from telegram import InlineKeyboardButton, MAX_MESSAGE_LENGTH, ParseMode, Bot
-from Yone import NO_LOAD
+from typing import List, Dict
+
+from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode,Update
 from telegram.error import TelegramError
 
+from Yone import NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -15,6 +16,27 @@ class EqInlineKeyboardButton(InlineKeyboardButton):
 
     def __gt__(self, other):
         return self.text > other.text
+
+
+def split_message(msg: str) -> List[str]:
+    if len(msg) < MAX_MESSAGE_LENGTH:
+        return [msg]
+
+    else:
+        lines = msg.splitlines(True)
+        small_msg = ""
+        result = []
+        for line in lines:
+            if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
+                small_msg += line
+            else:
+                result.append(small_msg)
+                small_msg = line
+        else:
+            # Else statement at the end of the for loop, so append the leftover string.
+            result.append(small_msg)
+
+        return result
 
 
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
@@ -30,30 +52,47 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              in module_dict.values()])
 
     pairs = [
-    modules[i * 2:(i + 1) * 2] for i in range((len(modules) + 2 - 1) // 2)
+    modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
     ]
 
-    round_num = len(modules) / 2
+    round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
     if calc == 1:
         pairs.append((modules[-1], ))
     elif calc == 2:
         pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) / 1)
+    max_num_pages = ceil(len(pairs) / 6)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
     if len(pairs) > 6:
         pairs = pairs[modulo_page * 6:6 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back", callback_data="yone_back"),
+                EqInlineKeyboardButton("「 MENU 」", callback_data="yone_back"),
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="yone_back")]]
+        pairs += [[EqInlineKeyboardButton("Home", callback_data="yone_back")]]
+
+
 
     return pairs
+
+
+def send_to_list(bot: Bot, send_to: list, message: str, markdown=False, html=False) -> None:
+    if html and markdown:
+        raise Exception("Can only send with either markdown or HTML!")
+    for user_id in set(send_to):
+        try:
+            if markdown:
+                bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN)
+            elif html:
+                bot.send_message(user_id, message, parse_mode=ParseMode.HTML)
+            else:
+                bot.send_message(user_id, message)
+        except TelegramError:
+            pass  # ignore users who fail
 
 
 def build_keyboard(buttons):
@@ -71,11 +110,16 @@ def revert_buttons(buttons):
     res = ""
     for btn in buttons:
         if btn.same_line:
-            res += f"\n[{btn.name}](buttonurl://{btn.url}:same)"
+            res += "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
         else:
-            res += f"\n[{btn.name}](buttonurl://{btn.url})"
+            res += "\n[{}](buttonurl://{})".format(btn.name, btn.url)
 
     return res
+
+def sendMessage(text: str, bot: Bot, update: Update):
+    return bot.send_message(update.message.chat_id,
+                                    reply_to_message_id=update.message.message_id,
+                                    text=text, parse_mode=ParseMode.HTML)
 
 def build_keyboard_parser(bot, chat_id, buttons):
     keyb = []
@@ -89,40 +133,7 @@ def build_keyboard_parser(bot, chat_id, buttons):
 
     return keyb
 
-def send_to_list(
-    bot: Bot, send_to: list, message: str, markdown=False, html=False,
-) -> None:
-    if html and markdown:
-        raise Exception("Can only send with either markdown or HTML!")
-    for user_id in set(send_to):
-        try:
-            if markdown:
-                bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN)
-            elif html:
-                bot.send_message(user_id, message, parse_mode=ParseMode.HTML)
-            else:
-                bot.send_message(user_id, message)
-        except TelegramError:
-            pass  # ignore users who fail
-
-def split_message(msg: str) -> List[str]:
-    if len(msg) < MAX_MESSAGE_LENGTH:
-        return [msg]
-
-    lines = msg.splitlines(True)
-    small_msg = ""
-    result = []
-    for line in lines:
-        if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
-            small_msg += line
-        else:
-            result.append(small_msg)
-            small_msg = line
-    else:
-        # Else statement at the end of the for loop, so append the leftover string.
-        result.append(small_msg)
-
-    return result
-
 def is_module_loaded(name):
-    return name not in NO_LOAD
+     return name not in NO_LOAD
+                
+    
